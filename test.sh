c789df06b5a1fa6e2b47614a8ec7cf2748cb37b1
sonota88 <yosiot8753@gmail.com>
2022-09-03 14:32:14 +0900

    test: Extract test_{compile,all} (test.sh)

--- test.sh
+++ test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-set -o errexit
+set -o nounset
+
+test_compile() {
+  ./test_compile.sh "$@"
+}
+
+test_all() {
+  ./test_lex.sh "$@"
+  ./test_compile.sh "$@"
+}
 
 cmd="$1"; shift
 case $cmd in
   compile | c*)
-    ./test_compile.sh "$@"
+    test_compile "$@"
     ;;
   all | a*)
-    ./test_lex.sh
-    ./test_compile.sh
+    test_all "$@"
     ;;
 esac
